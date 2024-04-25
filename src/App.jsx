309ab02b3344a8816979056ef5a06ed587ad6652
ken-85 <kwlepchabizness@gmail.com>
2024-04-25 20:30:57 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "ATIK",
+    name: "Kumar Harsh",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "SIDDHANT",
+    name: "Siddhanth Raj",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "NIHAL",
+    name: "Nehal Choudary",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
   {
     id: 499456,
-    name: "UJJAWAL",
+    name: "Suraj Aribenchi",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
